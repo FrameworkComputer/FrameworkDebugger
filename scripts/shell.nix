95e1886acf6fda72643ab0fc65f94c1093aaa62b
Daniel Schaefer <dhs@frame.work>
2026-03-14 18:50:16 +0800

    shell.nix: Add python deps for BusPirate-BPIO2-flatbuffer-interface
    `nix-shell` to get a shell to get them
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- scripts/shell.nix
+++ scripts/shell.nix
@@ -3,6 +3,7 @@
 (pkgs.buildFHSEnv {
   name = "framework-debugger-shell";
   targetPkgs = pkgs: with pkgs; [
+    (python3.withPackages (ps: [ ps.pyserial ps.flatbuffers ps.cobs ps.numpy ]))
     libftdi1
     libusb1
   ];
